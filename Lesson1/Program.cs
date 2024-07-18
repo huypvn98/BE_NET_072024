7bba321eb9e03803631f40686d6e29693c0f3449
DESKTOP-ET4TTVO\16520 <huypvn1998@gmail.com>
2024-07-18 23:52:28 +0700

    first commit

---
+++ Lesson1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write("Nhập số thứ nhất: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nhập số thứ hai: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = num1 + num2;
+            double product = num1 * num2;
+            double difference = num1 - num2;
+
+            Console.WriteLine("Tổng của hai số là: " + sum);
+            Console.WriteLine("Tích của hai số là: " + product);
+            Console.WriteLine("Hiệu của hai số là: " + difference);
+        }
+    }
+}
---
+++ Lesson2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            double a = 0;
+            double b = 0;
+            double x = 0;
+            loop:
+            Console.WriteLine("Giải phương trình bậc mấy: ");
+            switch (Convert.ToDouble(Console.ReadLine()))
+            {
+                case 1:
+                    // Giải phương trình bậc nhất
+                    Console.Write("Nhập a: ");
+                        a = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Nhập b: ");
+                        b = Convert.ToDouble(Console.ReadLine());
+
+                    if (a == 0)
+                    {
+                        if (b == 0)
+                            Console.WriteLine("Phương trình vô số nghiệm");
+                        else
+                            Console.WriteLine("Phương trình vô nghiệm");
+                    }
+                    else
+                    {
+                        x = -b / a;
+                        Console.WriteLine("Nghiệm của phương trình là: " + x);
+                    }
+                break;
+
+                case 2:
+                    // Giải phương trình bậc hai
+                    Console.Write("Nhập a: ");
+                    a = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Nhập b: ");
+                    b = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Nhập c: ");
+                    double c = Convert.ToDouble(Console.ReadLine());
+
+                    double delta = b * b - 4 * a * c;
+                    if (delta < 0)
+                    {
+                        Console.WriteLine("Phương trình vô nghiệm");
+                    }
+                    else if (delta == 0)
+                    {
+                        x = -b / (2 * a);
+                        Console.WriteLine("Phương trình có nghiệm kép x1 = x2 = " + x);
+                    }
+                    else
+                    {
+                        double sqrtDelta = Math.Sqrt(delta);
+                        double x1 = (-b + sqrtDelta) / (2 * a);
+                        double x2 = (-b - sqrtDelta) / (2 * a);
+                        Console.WriteLine("Phương trình có hai nghiệm phân biệt x1 = " + x1 + ", x2 = " + x2);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Giá trị không hợp lệ! Vui lòng nhập lại.");
+                    goto loop;
+            }                     
+        }
+    }
+}
